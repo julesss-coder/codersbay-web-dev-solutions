9a3980ca9d76aedde55001509555bf1a6bb31efc
julesss-coder <julesss@posteo.de>
2023-03-21 21:12:14 +0100

    Added calculations for all fields, added styles

--- maths-quiz/scripts/script.js
+++ maths-quiz/scripts/script.js
@@ -1,18 +1,19 @@
 // DOM elements
 let board = document.querySelector(".board");
-let fields = document.querySelectorAll(".field");
-let field1 = document.querySelector(".field-1");
-let field2 = document.querySelector(".field-2");
-let field3 = document.querySelector(".field-3");
-let field4 = document.querySelector(".field-4");
-let field5 = document.querySelector(".field-5");
-let field6 = document.querySelector(".field-6");
-let field7 = document.querySelector(".field-7");
-let field8 = document.querySelector(".field-8");
-let field9 = document.querySelector(".field-9");
+let fields = document.querySelectorAll(".field"); // NodeList
+// let field1 = document.querySelector(".field-1");
+// let field2 = document.querySelector(".field-2");
+// let field3 = document.querySelector(".field-3");
+// let field4 = document.querySelector(".field-4");
+// let field5 = document.querySelector(".field-5");
+// let field6 = document.querySelector(".field-6");
+// let field7 = document.querySelector(".field-7");
+// let field8 = document.querySelector(".field-8");
+// let field9 = document.querySelector(".field-9");
 
 
 let playerInputField = document.querySelector(".player-input-field");
+let questionAnswer = document.querySelector(".question-answer");
 let mathsQuestion = document.querySelector(".maths-question");
 let playerInput = document.querySelector("#player-input");
 let submitBtn = document.querySelector("#submit-btn");
@@ -67,17 +68,81 @@ class MathsQuiz {
     this.a = this.generateRandomInt();
     this.b = this.generateRandomInt();
     this.calculationString = `${this.a} + ${this.b} = ?`;
-    console.log("calculation string: ", this.calculationString);
     this.displayCalculation();
-    // Get actual result of calculation
     this.result = this.a + this.b;
-    
     this.checkLevel();
   }
-  // rest of calculation methods
-  
+
+  subtract() {
+    this.a = this.generateRandomInt();
+    this.b = this.generateRandomInt();
+    this.calculationString = `${this.a} - ${this.b} = ?`;
+    this.displayCalculation();
+    this.result = this.a - this.b;
+    this.checkLevel();
+  }
+
+  multiply() {
+    this.a = this.generateRandomInt();
+    this.b = this.generateRandomInt();
+    this.calculationString = `${this.a} * ${this.b} = ?`;
+    this.displayCalculation();
+    this.result = this.a * this.b;
+    this.checkLevel();
+  }
+
+  divide() {
+    this.a = this.generateRandomInt();
+    this.b = this.generateRandomInt();
+    this.calculationString = `${this.a} ÷ ${this.b} = ?`;
+    this.displayCalculation();
+    this.result = +(this.a / this.b).toFixed(3);
+    this.checkLevel();
+  }
+
+  square() {
+    this.a = this.generateRandomInt();
+    this.calculationString = `${this.a}² = ?`;
+    this.displayCalculation();
+    this.result = Math.pow(this.a, 2);
+    this.checkLevel();
+  }
+
+  cube() {
+    this.a = this.generateRandomInt();
+    this.calculationString = `${this.a}³ = ?`;
+    this.displayCalculation();
+    this.result = Math.pow(this.a, 3);
+    this.checkLevel();
+  }
+
+  getPercentage() {
+    this.a = this.generateRandomInt();
+    this.b = this.generateRandomInt();
+    this.calculationString = `${this.a}% * ${this.b} = ?`;
+    this.displayCalculation();
+    this.result = +((this.a)/100 * this.b).toFixed(3);
+    this.checkLevel();
+  }
+
+  getSquareRoot() {
+    this.a = this.generateRandomInt();
+    this.calculationString = `\u221A${this.a}`; // Does this work?
+    this.displayCalculation();
+    this.result = +(Math.sqrt(this.a)).toFixed(3);
+    this.checkLevel();
+  }
+
+  getCubeRoot() {
+    this.a = this.generateRandomInt();
+    this.calculationString = `\u221B${this.a}`; // Does this work?
+    this.displayCalculation();
+    this.result = +(Math.cbrt(this.a)).toFixed(3);
+    this.checkLevel();
+  }
+
   generateRandomInt() {
-    return Math.round(Math.random() * 101);
+    return Math.round(Math.random() * 51);
   }
   
   displayCalculation() {
@@ -91,7 +156,8 @@ class MathsQuiz {
       playerInput.classList.toggle("invalid-input");
       return; // What happens afterwards?
     } else {
-      return +playerInput.value
+      let playerInput = +this.playerInput;
+      return +playerInput.toFixed(3);
     }
   }
 
@@ -116,54 +182,52 @@ class MathsQuiz {
   }
 
   checkLevel() {
-    if (this.level >= 9) {
+    if (this.level > 9) {
       announcement.textContent = "You have successfully completed all levels. Congratulations!";
       makeMoveBtn.disabled = true;
       return; // End game
     }
   }
-
+  
   markCurrentLevel() {
-    let currentField = `field${this.level}`;
-    let previousField = `field${this.level - 1}`;
+    // get the field in fields for the current level:
+      // if level is 1, get field at index level - 1
+    
+    let currentField = fields[this.level - 1];
     // FIXME
-    // if (this.level > 1) {
-    //   currentField.classList.toggle("current-level");
-    // }
-    // previousField.classList.toggle("current-level");
+    if (this.level > 1) {
+      let previousField = fields[this.level - 2];
+      previousField.classList.remove("current-level");
+    }
+    if (!currentField.classList.contains("current-field")) {
+      currentField.classList.add("current-level");
+    }
   }
+
+
 }
 
 // Quiz instance
 let newQuiz = new MathsQuiz();
 
 // Event listeners
-// Field 1: on click: 
-  // generate random ints a, b
-  // call add(a, b)
-
-// "Make move" button
-/*
-===========CONTINUE HERE=========
-On click:
-  mark current level // where else am I doing this, and is it necessary?
-  askMathsQuestionForLevel() 
   
-*/
 makeMoveBtn.addEventListener("click", function() {
+  playerInput.focus(); // FIXME Why can't I add focus to input field?
   newQuiz.markCurrentLevel(); // adapt function for level 1 - there is no previous level
+  announcement.textContent = "";
+  mathsQuestion.textContent = "";
   newQuiz.askMathsQuestionForLevel();
   playerInputField.classList.toggle("hide");
-})
+});
 
 // Input field:
-// Should I have access to class instance here?
-// If the game were a set of regular functions, I would call the appropriate one from the event listener, too.
-// On hitting enter AND on hitting submit
 submitBtn.addEventListener("click", function(event) {
   newQuiz.evaluatePlayerInput();
   newQuiz.checkLevel();
-  playerInputField.classList.toggle("hide");
+  // if (!playerInputField.classList.contains("hide")) {
+  //   playerInputField.classList.add("hide");
+  // }; 
 });
 
 playerInput.addEventListener("keyup", function(event) {
@@ -171,10 +235,31 @@ playerInput.addEventListener("keyup", function(event) {
     console.log("event.target.value: ", event.target.value);
     newQuiz.evaluatePlayerInput();
     newQuiz.checkLevel();
-    playerInputField.classList.toggle("hide");
+    
+    // if (!playerInputField.classList.contains("hide")) {
+    //   playerInputField.classList.add("hide");
+    // }
   }
+});
+
+newGameBtn.addEventListener("click", function() {
+  mathsQuestion.textContent = "";
+  announcement.textContent = "";
+
+  // if (!playerInputField.classList.contains("hide")) {
+  //   playerInputField.classList.add("hide");
+  // }
+
+  fields.forEach((field) => {
+    field.classList.remove("current-level");
+  });
+  newQuiz = new MathsQuiz(); // is the previous instance deleted? Does the garbage collector do that?
 });
 
 
 
 // Use jQuery to traverse the DOM
+
+// Board changes display to 2 columns under 850px!!!
+// Find better way of hiding and displaying input field
+  // Currently, .hide is toggled every time player clicks "make move" (even when they don't enter a solution)
